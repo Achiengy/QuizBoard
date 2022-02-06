009c67b5928c7300050ae6f7e60fe35cf27e697f
Achiengy <hope.pete@student.moringaschool.com>
2022-02-06 15:33:19 +0300

    ft:UserInterface logic-This will guide the quiz on which answers are correct

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 // USER INTERFACE LOGIC
-function submit(){
-    var score = 0;
-    var correctAnswer = document.quiz.question1.value;
-    if (correctAnswer == "False")(score +=1)
-    var correctAnswer1 = document.quiz.question1.value;
-    var correctAnswer2 = document.quiz.question2.value;
-    var correctAnswer3 = document.quiz.question3.value;
-    var correctAnswer4 = document.quiz.question4.value;
-    var correctAnswer5 = document.quiz.question5.value;
-    if (correctAnswer1 == "False")(score +=1);
-    if (correctAnswer2 == "Alert("Hello");")(score +=1);
-    if (correctAnswer3 == "function = myfunction()")(score +=1);
-    if (correctAnswer4 == "if (i==5)")(score +=1);
-    if (correctAnswer5 == "var colors = ["red","green","blue"]")(score +=1);
-    document.write(score);
+function onSubmit(){
+    var score=0;
+    var numOfQuestions = 5;
+    var ansArr = ['B','B','C','C','A'];
+
+    var question1 = document.forms['quiz']['question1'].value;
+    var question2 = document.forms['quiz']['question2'].value;
+    var question3 = document.forms['quiz']['question3'].value;
+    var question4 = document.forms['quiz']['question4'].value;
+    var question5 = document.forms['quiz']['question5'].value;
+
+     for(var i = 1; i <= numOfQuestions; i++){
+         if(eval('quesion' + i) == ''){
+             alert("You missed question number " + i);
+        }
+
+        for(var i =1; i <= numOfQuestions)
+     }
 }
